608e5e42686ee289e8a564dbcb02c664b76ecdfa
brunoluiz <bruno_d_luiz@estudante.sesisenai.org.br>
2026-08-12 12:07:56 -0300

    add - a

--- php/index.php
+++ php/index.php
@@ -1,3 +1,22 @@
-<link rel="stylesheet" href="style.css"> 
+<?php
 
+include "../infra/conexao.php";
 
+$titulo = $_POST["titulo"];
+$autor = $_POST["autor"];
+$ano = $_POST["ano"];
+
+$stmt = $conexao->prepare(
+    "INSERT INTO livros (titulo, autor, ano)
+     VALUES (?, ?, ?)"
+);
+
+$stmt->bind_param("ssi", $titulo, $autor, $ano);
+
+$stmt->execute();
+
+$stmt->close();
+
+header("Location: ../index.php");
+
+?>
